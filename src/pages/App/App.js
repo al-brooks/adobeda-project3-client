bfecad9f824d3633394041844aa90256e393506e
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-15 13:58:20 -0700

    wip: adds browser router + init routes

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import SideBar from "../../components/SideBar/SideBar";
+import BookListPage from "../BookListPage/BookListPage";
+import BookDetailPage from "../BookDetailPage/BookDetailPage";
 
 function App() {
   return (
     <div className="App">
-      <p>App</p>
+      <NavBar />
+      <SideBar />
+      <Routes>
+        <Route path="/books" element={<BookListPage />} />
+        <Route path="/books/details" element={<BookDetailPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
