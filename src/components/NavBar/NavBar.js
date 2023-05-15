d7192e41cedff4c257f99b7b3b0639e3f73374ec
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-15 16:02:29 -0700

    wip: adds nav skeleton

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,3 +1,16 @@
 export default function NavBar() {
-  return <h2>NavBar</h2>;
+  return (
+    <nav>
+      <p>LOGO</p>
+      <form>
+        <select name="search-options">
+          <option value="Title">Title</option>
+          <option value="Author">Author</option>
+          <option value="Subject">Subject</option>
+        </select>
+        <input type="text" placeholder="Search..." />
+        <button>search</button>
+      </form>
+    </nav>
+  );
 }
