7052888f13c39b4e6288bc3cd6137eef20b4417b
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-15 14:38:12 -0700

    wip: adds landing pg + skeleton

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import SideBar from "../SideBar/SideBar";
+import LandingPage from "../../pages/LandingPage/LandingPage";
 import BookListPage from "../../pages/BookListPage/BookListPage";
 import BookDetailPage from "../../pages/BookDetailPage/BookDetailPage";
 
@@ -12,6 +13,7 @@ function App() {
       <NavBar />
       <SideBar />
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/books" element={<BookListPage />} />
         <Route path="/books/:id" element={<BookDetailPage />} />
       </Routes>
