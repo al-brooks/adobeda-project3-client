2a5a5e81703fbb93998fc102dc6f9b27c184baa5
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-16 11:48:04 -0700

    wip: request book data from api

--- src/pages/SearchResultPage/SearchResultPage.js
+++ src/pages/SearchResultPage/SearchResultPage.js
@@ -1,3 +1,36 @@
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 export default function SearchResultPage() {
-  return <h2>SearchResultPage</h2>;
+  const [books, setBooks] = useState({});
+
+  const search = useLocation().search;
+  const apiUrl = "https://openlibrary.org/search.json";
+
+  const fetchBooks = async () => {
+    try {
+      const response = await fetch(`${apiUrl}${search}`);
+      const bookData = await response.json();
+      setBooks(bookData.docs);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  console.log(books);
+
+  return (
+    <main>
+      <h2>Search Result Page</h2>
+      <p>Below are the results:</p>
+    </main>
+  );
 }
+
+// Pulling search params
+// const params = new URLSearchParams(search);
+// for (const [key, value] of params) {
+//   console.log(`${key}: ${value}`);
+// }
