a81a91e2a5ad471d55ee401138cd3a4ed939d8be
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-16 09:58:03 -0700

    wip: adds in searchParams

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,24 +1,47 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, createSearchParams } from "react-router-dom";
 
 export default function NavBar({ setSearch }) {
+  const [newSearch, setNewSearch] = useState({
+    searchOption: "title",
+    searchText: ""
+  });
+
   const navigate = useNavigate();
+
+  const handleChange = evt => {
+    setNewSearch({
+      ...newSearch,
+      [evt.target.name]: evt.target.value
+    });
+  };
+
   const handleSubmit = evt => {
     evt.preventDefault();
-    console.log("test");
-    navigate("/books");
+    navigate({
+      pathname: "search",
+      search: createSearchParams({
+        title: "Lord of the Rings",
+        language: "eng"
+      }).toString()
+    });
   };
 
   return (
     <nav>
       <p>LOGO</p>
       <form onSubmit={handleSubmit}>
-        <select name="search-options">
-          <option value="Title">Title</option>
-          <option value="Author">Author</option>
-          <option value="Subject">Subject</option>
+        <select name="searchOption" onChange={handleChange}>
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="subject">Subject</option>
         </select>
-        <input type="text" placeholder="Search..." />
+        <input
+          onChange={handleChange}
+          name="searchText"
+          type="text"
+          placeholder="Search..."
+        />
         <button type="submit">search</button>
       </form>
     </nav>
