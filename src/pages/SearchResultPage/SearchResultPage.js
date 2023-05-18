69751d5687897907996b2a0ec2892152dfdca8f9
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-18 16:47:49 -0700

    adds search results styles

--- src/pages/SearchResultPage/SearchResultPage.js
+++ src/pages/SearchResultPage/SearchResultPage.js
@@ -1,5 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 import { Link, useLocation } from "react-router-dom";
+import SideBar from "../../components/SideBar/SideBar";
+import "./SearchResultPage.css";
 
 export default function SearchResultPage() {
   const [books, setBooks] = useState({});
@@ -25,42 +27,46 @@ export default function SearchResultPage() {
 
   const loaded = () => {
     return (
-      <main>
-        <h2>Search Result Page</h2>
-        <p>Below are the results:</p>
-        {books.length > 0 ? (
-          books.map(book => {
-            let id = book.key.split("/")[2];
-            if (book.cover_i && book.ratings_average) {
-              return (
-                <article key={id}>
-                  <Link
-                    to={`/book/${id}`}
-                    state={{
-                      author: book.author_name[0],
-                      ratings_average: book.ratings_average.toFixed(2)
-                    }}
-                  >
-                    <img
-                      src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
-                      alt="book cover"
-                    />
-                  </Link>
-                  <Link to={`/book/${id}`}>
-                    <h3>{book.title}</h3>
-                  </Link>
-                  <h4>By: {book.author_name[0]}</h4>
-                  <p>First Published: {book.first_publish_year}</p>
-                  <p>Number of Editions: {book.editions.numFound}</p>
-                </article>
-              );
-            } else {
-              return <div key={id} style={{ display: "hidden" }}></div>;
-            }
-          })
-        ) : (
-          <p>No Books Match Your Search...</p>
-        )}
+      <main className="SearchResultPage">
+        <section>
+          <h2>Search Result Page</h2>
+          {books.length > 0 ? (
+            books.map(book => {
+              let id = book.key.split("/")[2];
+              if (book.cover_i && book.ratings_average) {
+                return (
+                  <article className="flex-ctr-ctr" key={id}>
+                    <Link
+                      to={`/book/${id}`}
+                      state={{
+                        author: book.author_name[0],
+                        ratings_average: book.ratings_average.toFixed(2)
+                      }}
+                    >
+                      <img
+                        src={`https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`}
+                        alt="book cover"
+                      />
+                    </Link>
+                    <section className="book-info">
+                      <Link to={`/book/${id}`}>
+                        <h3>{book.title}</h3>
+                      </Link>
+                      <h4>By: {book.author_name[0]}</h4>
+                      <p>First Published: {book.first_publish_year}</p>
+                      <p>Number of Editions: {book.editions.numFound}</p>
+                    </section>
+                  </article>
+                );
+              } else {
+                return <div key={id} style={{ display: "hidden" }}></div>;
+              }
+            })
+          ) : (
+            <p>No Books Match Your Search...</p>
+          )}
+        </section>
+        <SideBar />
       </main>
     );
   };
