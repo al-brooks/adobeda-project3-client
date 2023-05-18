fa90846d2b14af338e455bf0db1fcb5911cd6bf0
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-18 14:51:25 -0700

    wip: adds side info styles

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,18 @@
+import "./SideBar.css";
 import Footer from "../Footer/Footer";
 
 export default function SideBar() {
   return (
     <section id="about">
-      <h2>BookSmart</h2>
-      <p>
-        Is a website that utilizes the Openlibrary API to provide users with the
-        ability to search for their favorite books...
-      </p>
+      <section id="site-info">
+        <h2>
+          Book<span>Smart</span>
+        </h2>
+        <p>
+          Is a website that utilizes the Openlibrary API to provide users with
+          the ability to search for their favorite books...
+        </p>
+      </section>
       <Footer />
     </section>
   );
