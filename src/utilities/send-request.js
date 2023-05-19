ab713978d68c9b38d7815d12f93687598782498a
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-18 18:19:12 -0700

    updates try catch

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,11 +1,13 @@
 export default async function bookRequest(url, method = "GET", payload = null) {
-  const serverUrl = `http://localhost:3001`;
-  const options = { method };
-  // if payload add to options
+  try {
+    const serverUrl = `http://localhost:3001`;
+    const options = { method };
+    // if payload add to options
 
-  const res = await fetch(serverUrl + url, options);
-  const book = await res.json();
-
-  if (res.ok) return book;
-  throw new Error("Bad Request");
+    const res = await fetch(serverUrl + url, options);
+    const book = await res.json();
+    return book;
+  } catch {
+    throw new Error("Bad Request");
+  }
 }
