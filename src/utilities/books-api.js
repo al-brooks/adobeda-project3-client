f03167d2d6cbc6a6803e29269184a3f7e113ed34
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-18 18:55:49 -0700

    updates server url

--- src/utilities/books-api.js
+++ src/utilities/books-api.js
@@ -1,5 +1,5 @@
 import bookRequest from "./send-request";
-const BASE_URL = "http://localhost:3001/api/books";
+const BASE_URL = "https://book-smart.herokuapp.com/api/books";
 
 export function getBook(bookId) {
   return bookRequest(`${BASE_URL}/${bookId}`);
