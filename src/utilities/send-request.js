e669211e3940e3d734ca203e63f46a5d9af98a82
Alexander Brooks <alexanderlawbrooks@gmail.com>
2023-05-18 18:21:10 -0700

    updates request paramters

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,10 +1,9 @@
-export default async function bookRequest(url, method = "GET", payload = null) {
+export default async function bookRequest(bookUrl, method = "GET") {
   try {
     const serverUrl = `http://localhost:3001`;
     const options = { method };
-    // if payload add to options
 
-    const res = await fetch(serverUrl + url, options);
+    const res = await fetch(serverUrl + bookUrl, options);
     const book = await res.json();
     return book;
   } catch {
